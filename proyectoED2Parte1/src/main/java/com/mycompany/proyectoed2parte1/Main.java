3ad7879733297c4bbbfaad90a5078d25e7911155
RobertoPatino1 <rapatino@espol.edu.ec>
2022-07-29 21:19:15 -0500

    Creacion de estructura inicial del proyecto, clases y metodos para leer los archivos de preguntas y respuestas

--- proyectoED2Parte1/src/main/java/com/mycompany/proyectoed2parte1/Main.java
+++ proyectoED2Parte1/src/main/java/com/mycompany/proyectoed2parte1/Main.java
@@ -1,10 +1,17 @@
 package com.mycompany.proyectoed2parte1;
 
+import util.Constants;
+import util.Lector;
+
 
 public class Main {
 
     public static void main(String[] args) {
-
+        System.out.println(Lector.cargarListaPreguntas(Constants.rutaPreguntas));
+        
+        System.out.println(Lector.cargarListaRespuestas(Constants.rutaRespuestas));
+        
+        
     }
     
 }
